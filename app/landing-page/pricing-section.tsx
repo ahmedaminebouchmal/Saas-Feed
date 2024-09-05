f70ee90b8cc638ce5308b23d60876e0a0e697427
ahmedaminebouchmal <bouchmalaabesp@gmail.com>
2024-09-06 00:13:17 +0200

    price cnfg

--- app/landing-page/pricing-section.tsx
+++ app/landing-page/pricing-section.tsx
@@ -25,7 +25,7 @@ export const pricingPlans: PricingPlan[] = [
   },
   {
     title: "Monthly",
-    price: 6.99,
+    price: 9.99,
     description: "For growing teams",
     isPopular: true,
     url: "/payments/subscribe?plan=monthly",
@@ -38,7 +38,7 @@ export const pricingPlans: PricingPlan[] = [
   },
   {
     title: "Yearly",
-    price: 39.99,
+    price: 59.99,
     description: "Upgrade to save more!",
     isPopular: false,
     url: "/payments/subscribe?plan=yearly",
