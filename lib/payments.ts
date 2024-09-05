87d574af441e3eee59954d0bfee1fb27a5c75469
ahmedaminebouchmal <bouchmalaabesp@gmail.com>
2024-09-06 00:02:45 +0200

    vercel cnfg

---
+++ lib/payments.ts
@@ -0,0 +1,3 @@
+export const monthlyPlanId: string = "price_1PvmsQDIydOGDtX0aVI5xIeR";
+export const yearlyPlanId: string = "price_1PvmuXDIydOGDtX0wZYhFsu5";
+export const maxFreeProjects: number = 1;
